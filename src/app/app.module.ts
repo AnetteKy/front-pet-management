3797d0c3c0a6f18f9e585a5f44398090e82fce23
AnetteKy <anette.kytt@gmail.com>
2023-05-07 15:14:08 +0300

    added bootstrap for design and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PetsListComponent } from './pets-list/pets-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetsListComponent
   ],
   imports: [
     BrowserModule
